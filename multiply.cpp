a517ec2187573d8ef1aac408d6495fe8415a82f1
Devansh Chauhan <chauhandevansh9@gmail.com>
2023-03-05 13:09:20 +0530

    multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main()
+{
+    int a,b;
+    cout<<"Enter A : ";
+    cin>>a;
+    cout<<"Enter B : ";
+    cin>>b;
+    cout<<"Product of A and B : "<<(a*b);
+    return 0;
+}
