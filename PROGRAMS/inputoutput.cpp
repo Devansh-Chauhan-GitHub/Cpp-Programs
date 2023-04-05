e0e03468bd9177aeab317003dbe626c7324340e0
Devansh-Chauhan-GitHub <chauhandevansh9@gmail.com>
2023-04-05 22:18:23 +0530

    .

---
+++ PROGRAMS/inputoutput.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the value of a : ";
+    cin>>a;
+    cout<<"";
+    cout<<"THe value of a is : "<<a;
+    system("pause");
+    return 0;
+}
